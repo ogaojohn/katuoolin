96d3ffee6115c7c94a3d11e49ba33634b3c1a7cd
ogaojohn <96224812+ogaojohn@users.noreply.github.com>
2025-06-30 23:36:32 +0300

    Update katuoolin.py

--- katuoolin.py
+++ katuoolin.py
@@ -49,7 +49,7 @@ $$ | \$$\ \$$$$$$$ |  \$$$$  | $$$$$ ||\$$$$$$  |\$$$$$$  |$$ |$$ |$$ |  $$ |
 					''')
 					repo = input("\033[1;32mWhat do you want to do ?> \033[1;m")
 					if repo == "1":
-						cmd1 = os.system("apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ED444FF07D8D0BF6")
+						cmd1 = os.system("apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 603374c107a90a69d983dbcb4d31e0d6eedfc325")
 						cmd2 = os.system("echo '# Kali linux repositories | Added by katuoolin\ndeb http://http.kali.org/kali kali-rolling main contrib non-free' >> /etc/apt/sources.list")
 					elif repo == "2":
 						cmd3 = os.system("apt-get update -m")
